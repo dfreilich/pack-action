977b114159da484afd46fb4f529a38fa59905a3c
David Freilich <dfreilich@vmware.com>
2020-09-08 20:52:28 +0300

    Remove docker auth, and switch to using docker/login-action instead
    Signed-off-by: David Freilich <dfreilich@vmware.com>

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM docker:stable-dind
-
-COPY --from=buildpacksio/pack /usr/local/bin/pack /usr/local/bin/pack
-COPY entrypoint.sh /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ -n "${INPUT_USERNAME}" ] && [ -n "${INPUT_PASSWORD}" ]; then
-  echo "${INPUT_PASSWORD}" | docker login -u "${INPUT_USERNAME}" --password-stdin "${INPUT_REGISTRY}"
-fi
-eval "pack ${INPUT_ARGS}"
