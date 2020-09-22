bca135fe1908eb96e68c61ca58f5f9081c1e47c2
David Freilich <dfreilich@vmware.com>
2020-09-22 15:33:15 +0300

    Use container to pass along /home/runner, and allow access to docker config
    Signed-off-by: David Freilich <dfreilich@vmware.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 echo $HOME
 ls -al $HOME/.docker
 cat ~/.docker/config.json
-cat /home/runner/.docker/config.json
+cat /var/www/.docker/config.json
+cp /var/www/.docker/config.json ~/.docker/config.json
+cat ~/.docker/config.json
 
 eval "pack ${INPUT_ARGS}"
